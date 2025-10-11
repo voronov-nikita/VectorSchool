6a26f305f2f188b6e68d425dae59d3e8fde0d8b5
voronov-nikita <voronovnr_1@mail.ru>
2025-10-11 13:42:52 +0300

    add new branch for testing new functions

--- bot/config.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = getenv("BOT_TOKEN")
-ADMIN_IDS = list(map(int, getenv("ADMIN_IDS", "").split(',')))
-DATABASE_NAME = "achievements.db"
